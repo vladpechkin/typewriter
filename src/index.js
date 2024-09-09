1e3184c2fd7efe19cd3574b9c610077169810de4
Vlad Pechkin <pechkinde@gmail.com>
2024-09-09 18:59:08 +0400

    Add tutorial

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useMemo, useState } from "react";
+import bellMp3 from "./bell.mp3";
+import {
+  CHAR_WIDTH,
+  CHAR_HEIGHT,
+  CHARS_IN_LINE,
+  TUTORIAL_EN,
+  TUTORIAL_RU,
+} from "./consts";
 
 const App = () => {
-  const [charsXOffset, setCharsXOffset] = useState(0);
-  const [charsYOffset, setCharsYOffset] = useState(0);
+  const [xOffset, setXOffset] = useState(0);
+  const [yOffset, setYOffset] = useState(CHAR_HEIGHT * 25);
+
+  const [text, setText] = useState("");
 
   const ref = useRef(null);
 
-  const CHARACTER_WIDTH = 8.795;
-  // const CHARACTER_HEIGHT = 20;
+  const [isTutorialComplete, setIstutorialComplete] = useState(false);
 
-  const CHARACTERS_IN_LINE = 72;
+  const [padding, setPadding] = useState(2);
+
+  const bell = useMemo(() => new Audio(bellMp3), []);
 
   const keyHandler = useCallback(
     (event) => {
-      if (event.key.includes("Arrow")) {
-        event.preventDefault();
-        return;
+      if (!isTutorialComplete) {
+        setIstutorialComplete(true);
+        setText("");
+        setYOffset(0);
+      }
+
+      if (Math.round(xOffset) === Math.round(CHAR_WIDTH * 62)) {
+        bell.play();
       }
 
       if (
@@ -30,63 +46,91 @@ const App = () => {
         return;
 
       if (
-        Math.round(charsXOffset) ===
-          Math.round(CHARACTER_WIDTH * CHARACTERS_IN_LINE) &&
-        event.key !== "Enter"
+        (Math.round(xOffset) === Math.round(CHAR_WIDTH * CHARS_IN_LINE) &&
+          event.key !== "Enter") ||
+        (xOffset < 1 &&
+          (event.key === "Backspace" || event.key === "ArrowLeft")) ||
+        yOffset > 1000
       ) {
         event.preventDefault();
-
-        // setCharsXOffset(CHARACTER_WIDTH);
 
         return;
       }
 
       if (event.key === "Backspace" || event.key === "ArrowLeft") {
-        if (charsXOffset === 0) setCharsXOffset(ref?.current?.clientWidth);
+        if (xOffset === 0) setXOffset(ref?.current?.clientWidth);
 
-        if (charsXOffset > 0) setCharsXOffset(charsXOffset - CHARACTER_WIDTH);
+        if (xOffset > 0) setXOffset(xOffset - CHAR_WIDTH);
 
         return;
       }
 
       if (event.key.length === 1 || event.key === "ArrowRight") {
-        setCharsXOffset(charsXOffset + CHARACTER_WIDTH);
+        setXOffset(xOffset + CHAR_WIDTH);
 
         return;
       }
 
-      // if (event.key === "ArrowUp") setCharsYOffset(charsYOffset + CHARACTER_HEIGHT)
+      if (event.key === "ArrowUp") setYOffset(yOffset - CHAR_HEIGHT);
 
-      setCharsXOffset(0);
+      if (event.key === "ArrowDown") setYOffset(yOffset + CHAR_HEIGHT);
+
+      if (event.key === "Enter") {
+        setXOffset(0);
+        setYOffset(yOffset + CHAR_HEIGHT);
+      }
+
+      return;
     },
-    [charsXOffset]
+    [xOffset, yOffset, bell, isTutorialComplete]
   );
 
   useEffect(() => {
-    ref.current.focus();
+    setText(`TYPEWRITER
+by @vladpechkin
+
+${navigator.language.includes("ru") ? TUTORIAL_RU : TUTORIAL_EN}`);
 
     document.addEventListener("keydown", keyHandler, false);
 
-    return () => document.removeEventListener("keydown", keyHandler, false);
+    return () => {
+      document.removeEventListener("keydown", keyHandler, false);
+    };
   }, [keyHandler]);
 
   return (
     <>
-      <div
-        contentEditable
+      <textarea
         autoFocus
         ref={ref}
+        value={text}
+        onChange={({ target }) => setText(target.value)}
+        focus={{ preventScroll: true }}
         style={{
-          left: `calc(50% - ${charsXOffset}px)`,
-          bottom: `calc(50% - 12px - ${charsYOffset}px)`,
+          padding: `${padding}cm`,
+          left: `calc(50% - ${padding}cm - ${CHAR_WIDTH / 2}px - ${xOffset}px)`,
+          top: `calc(100% - 2cm - 10px - ${CHAR_HEIGHT}px - ${yOffset}px)`,
         }}
-        className="input"
+        className="page"
         onBlur={({ target }) => target.focus()}
-      ></div>
-      <div className="cursor"></div>
-      {/* <div className="stats">
-        {charsXOffset + " " + ref?.current?.clientWidth}
-      </div> */}
+      ></textarea>
+      <div className="typewriter">
+        <div className="pointer" style={{ height: CHAR_HEIGHT }}></div>
+        <div className="scale">
+          {Array.from({ length: 22 }, () => (
+            <div className="mark"></div>
+          ))}
+
+          <div
+            className="finger"
+            style={{ left: `calc(${padding}cm - 2px)` }}
+          ></div>
+          <div
+            className="finger"
+            style={{ right: `calc(${padding}cm - 2px)` }}
+          ></div>
+        </div>
+      </div>
     </>
   );
 };
